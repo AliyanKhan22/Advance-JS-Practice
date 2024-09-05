c331f738c64ebdba6e4f49938e802b040577b702
AliyanKhan22 <alyanwasim81@gmail.com>
2024-09-05 11:29:56 -0700

    Advance JS Practice

---
+++ script.js
@@ -0,0 +1,102 @@
+ // Template Literals
+    // let a = "Aliyan"
+    // let b = "My Name is " + a
+    // console.log(b);
+
+
+    // let a = `My Name is Aliyan ${18} , ${22} and ${18 + 22}
+    // Adnan Khan` 
+    // console.log(a);
+    
+    
+
+    // Ternary Operators 
+    // let condition = true
+    // if(condition){
+    //   console.log("Condtion True");
+    // }else{
+    //     console.log("Condition Flase");
+    // }
+
+
+    // let condition = false
+
+    // ? console.log("Your Condition is True")
+    // : console.log("Your Condition is Flase");
+    
+
+    //   let age = 15
+    //   let StudentCard = true
+     
+    //   age >= 18
+    //   ? console.log("Allow")
+    //   : StudentCard
+    //   ? console.log("Allow On StudentCard")
+    //   : console.log("Not Allow");
+      
+
+
+   // PAss by Value and Pass by Refrence
+//    let obj = {
+//     Name: "Aliyan Khan",
+//     id: 2218
+//    }
+   
+//    let b = obj;
+//    b.inst = "SMIT"
+//    console.log(b);
+
+
+// let a = [1,2,3]
+// let b = a;
+// b.push("Aliyan Khan")
+// // b.pop()
+// console.log(a);
+
+
+
+// Spread Operators
+
+// let a = [1,2,3]
+// let b =[...a]
+// b.push("Khan")
+// console.log(a,b);
+
+
+// First Example 
+// let a = [1,2,3]
+// let b = ["Aliyan","Adnan","Khan"]
+// let c = [...a]
+// console.log(c);
+
+
+// let a = {
+//     Name:"Aliyan Khan",
+//     id:2218,
+// }
+// let b = {
+//     Inst:"SMIT"
+// }
+// let c = {
+//     ...a,...b
+// }
+// console.log(c);
+
+
+// Destructure Array and Objects
+
+// let a = [1,2,3,4]
+// let [index1 , index2 , ,index4] = a
+// console.log(index4);
+
+
+
+// let b = {
+//     Name:"Aliyan Khan",
+//     id:2218,
+//     Age:19,
+//     inst:"SMIT"
+// }
+// let {Name,id} = b
+// console.log(Name,id);
+
